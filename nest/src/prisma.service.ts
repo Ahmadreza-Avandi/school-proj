be301cf8aae92d92f6a0387c31686b93a2da0129
Ahmadrerza Avandi <143386424+Ahmadreza-Avandi@users.noreply.github.com>
2025-04-20 10:27:43 +0330

    رجیستر

--- nest/src/prisma.service.ts
+++ nest/src/prisma.service.ts
@@ -9,10 +9,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   constructor() {
     super({
       log: [
-        { emit: 'event', level: 'query' },
-        { emit: 'event', level: 'error' },
-        { emit: 'event', level: 'info' },
-        { emit: 'event', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
       ],
     });
   }
@@ -22,26 +21,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
       this.logger.log('Connecting to database...');
       await this.$connect();
       this.logger.log('Database connection established successfully');
-
-      // اضافه کردن لیسنرها برای رخدادهای مختلف
-      this.$on('error', (e) => {
-        this.logger.error(`Prisma Error: ${e.message}`, e.stack);
-      });
       
-      this.$on('query', (e) => {
-        this.logger.debug(`Prisma Query: ${e.query}`);
-      });
-      
-      this.$on('info', (e) => {
-        this.logger.log(`Prisma Info: ${e.message}`);
-      });
-      
-      this.$on('warn', (e) => {
-        this.logger.warn(`Prisma Warning: ${e.message}`);
-      });
-
     } catch (error) {
-      this.logger.error(`Failed to connect to the database: ${error.message}`, error.stack);
+      this.logger.error(`Failed to connect to the database: ${error.message}`);
       throw error;
     }
   }
